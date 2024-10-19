6e41e8219f73f1b64d81007c5738a52092ca2982
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-19 22:55:40 +0530

    h
    j

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main(){
 int n;
 int k = 5;
-n = (100 % k ? k + 1 : k - 1);
+n = (100 % k  ? k + 1 : k - 1); //  true =1 ; false= 0
 cout << "n = " << n << " k = " << k << endl;}
+
+
+
+
--- hi.cpp
+++ hi.cpp
@@ -2,7 +2,12 @@
 
 using  namespace std;
 int main(){
-    int n;
-    n =89;
-    cout << n << " \n"<< "for the end" <<endl;
+    int n = 5;
+    int y = 10;
+    int u ;
+    u=(y<n? y:n);
+    cout <<u;
 }
+
+
+ //condiiton ? true : false
